f70f1f607c26b30bfc5c6f55755fa766854ea4eb
Johannes Westlund <johannes@Johanness-MacBook-Pro.local>
2017-07-05 17:09:51 +0200

    Added some dgub code to find segementation error

--- heldKarp.cpp
+++ heldKarp.cpp
@@ -1,23 +1,32 @@
 /*
  * An implementation of the Held-Karp algorithm
  * that uses dynamic programming to solve
- * TSP in time complexity O(2^n * n ^2).
- * The implementation is based on the pseudocode
+ * TSP. The implementation is based on the pseudocode
  * found at https://en.wikipedia.org/wiki/Held–Karp_algorithm
  */
+
+//Define macros for debugging
+ #ifdef DEBUG
+ #define print(x) std::cout << x << std::endl
+ #else
+ #define print(x)
+ #endif
 
 #include <iostream>
 #include <string>
 #include <utility>
-#include <unordered_map>
-#include <unordered_set>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <limits>
 
-//std::unordered_map<std::pair<std::unordered_set<int>,int>,int> minPathCost;
-std::unordered_map< std::pair<int,int> ,int > edges;
-std::unordered_map<std::string,int> dictionary;
+std::map<std::pair<std::set<int>,int>,int> minPathCost;
+std::map<std::pair<int,int>,int> edges;
+std::map<std::string,int> dictionary;
 
 int main() {
-  std::string fromCity,toCity; int cities,cost,city = 0;
+  std::string fromCity,toCity; int cities,cost,city = 1;
   // Read graph
   std::cin >> cities;
   while (std::cin >> fromCity >> toCity >> cost) {
@@ -29,9 +38,62 @@ int main() {
     }
     edges.insert({std::make_pair(dictionary[fromCity],dictionary[toCity]),cost});
   }
-
+  print("Held-Karp start");
   //Held-Karp algorithm starts here
   //Based on pseudocode from https://en.wikipedia.org/wiki/Held–Karp_algorithm
+  std::vector<int> tot;
+  std::set<int> totSet;
+  tot.reserve(cities-2);
+  for (int k = 2; k <= cities; k++) {
+    std::set<int> tmp = {k};
+    tot[k-2] = k;
+    totSet.insert(k);
+    minPathCost.insert({std::make_pair(tmp,k),edges.at({1,k})});
+  }
+  print("Base case is done");
+
+  for (int subSize = 2; subSize < cities; subSize++) {
+    int skip = 0;
+    do {
+      if (skip == 0) {
+        std::set<int> subSet;
+        for (int taken = 0; taken < subSize; taken++) {
+          subSet.insert(tot[taken]);
+        }
+
+        for (auto it = subSet.cbegin(); it != subSet.cend(); it++) {
+          int minCost = std::numeric_limits<int>::max();
+          std::set<int> tmpSet (subSet);
+          tmpSet.erase(*it);
+          for (auto it2 = subSet.cbegin(); it != subSet.cend(); it2++) {
+            if (it2 != it) {
+              int tmpCost = minPathCost[std::make_pair(tmpSet,*it2)] + edges[std::make_pair(*it2,*it)];
+              if (minCost > tmpCost) {
+                minCost = tmpCost;
+              }
+            }
+          }
+          minPathCost.insert({std::make_pair(subSet,*it),minCost});
+        }
+
+        skip = [](int num)->int {int tmp = 1; for (int i = 1; i <= num; i++) tmp *= i; return tmp;}(cities-subSize);
+      }
+      --skip;
+    } while (std::next_permutation(tot.begin(),tot.end()));
+  }
+  print("Recursive steps are done");
+
+  int minCost = std::numeric_limits<int>::max();
+  for (auto it = tot.cbegin(); it != tot.cend(); it++) {
+    totSet.erase(*it);
+    int tmpCost = minPathCost[std::make_pair(totSet,*it)] + edges[std::make_pair(*it,1)];
+    if (minCost > tmpCost) {
+      minCost = tmpCost;
+    }
+    totSet.insert(*it);
+  }
+
+  std::cout << "The minimum cost to travel is " << minCost << std::endl;
 
   return 0;
 }
